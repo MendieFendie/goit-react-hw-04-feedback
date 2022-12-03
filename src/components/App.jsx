728e6842f3785f2a20fc772f61c6fd051590de64
Oleh Savaryn <olegsavaryn@gmail.com>
2022-12-03 23:30:12 +0100

    checkPoint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Buttons from './feedback/buttons';
+export default class App extends React.Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+
+  state = {
+    good: this.props.initialValue,
+    neutral: this.props.initialValue,
+    bad: this.props.initialValue,
+    total: this.props.initialValue,
+    percentage: this.props.initialValue,
+  };
+
+  handleGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  handleNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  handleBad = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    this.setState(prevState => {
+      return {
+        total: prevState.total + 1,
+      };
+    });
+  };
+
+  countPositiveFeedbackPercentage() {}
+
+  render() {
+    return (
+      <>
+        <h1>Please leave feedback</h1>
+        <Buttons
+          onGood={this.handleGood}
+          onNeutral={this.handleNeutral}
+          onBad={this.handleBad}
+        />
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+          <li>Total: {this.state.total}</li>
+          <li>Positive Percentage: {this.state.percentage}</li>
+        </ul>
+      </>
+    );
+  }
+}
---
+++ src/components/feedback/buttons.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Buttons = ({ onGood, onNeutral, onBad }) => {
+  return (
+    <>
+      <button type="button" onClick={onGood}>
+        Good
+      </button>
+      <button type="button" onClick={onNeutral}>
+        Neutral
+      </button>
+      <button type="button" onClick={onBad}>
+        Bad
+      </button>
+    </>
+  );
+};
+
+export default Buttons;
